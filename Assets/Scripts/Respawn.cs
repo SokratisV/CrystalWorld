3b4fabaeb5945757180c8bcb2656c6446c0ef46b
Sokratis Vogiatzakis <tp4412@edu.teicrete.gr>
2019-03-14 17:08:46 +0200

    Added audio

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            transform.position = respawnPoint.position;
+        }
+    }
+}
