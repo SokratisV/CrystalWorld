0cc358f03c49a9e1067d6f3064a16ae11724396a
Sokratis Vogiatzakis <tp4412@edu.teicrete.gr>
2019-03-16 23:19:05 +0200

    Improved village area, added more invisible walls, added teleport to spawn script.

---
+++ Assets/TeleportToSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TeleportToSpawn : MonoBehaviour
+{
+    public Transform spawnPoints;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Teleport(spawnPoints.GetChild(0));
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Teleport(spawnPoints.GetChild(1));
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Teleport(spawnPoints.GetChild(2));
+        }
+    }
+
+    private void Teleport(Transform point)
+    {
+        transform.position = point.position;
+    }
+}
